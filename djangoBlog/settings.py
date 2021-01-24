9128f33c708bf7bd208162e6a4bdbb2caf27a84d
far2naz <far2naz@gmail.com>
2021-01-24 16:52:58 +0330

    connect to my sql

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'djangoBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoblog_db',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
